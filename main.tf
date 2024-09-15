c5d09f9cde39ffebce2a595fabb3a69d7e339ba9
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-09-15 22:09:34 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -477,7 +477,7 @@ echo "WordPress secret keys have been updated successfully."
 
 exit 0
 EOF3
-chmod 700 ./change_salt; ./change_salt
+chmod 700 change_salt; ./change_salt
 
 cat <<EOF4 > install_wp
 #!/bin/bash
@@ -517,7 +517,7 @@ rm /tmp/wp_cookie_jar
 
 exit 0
 EOF4
-chmod 700 ./install_wp; ./install_wp
+chmod 700 install_wp; ./install_wp
 
 # Copy wp-config.php into the running WordPress container
 docker cp /tmp/wp-config.php wordpress-dev:/var/www/html/wp-config.php
