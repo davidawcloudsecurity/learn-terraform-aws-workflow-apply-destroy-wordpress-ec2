52cfcbd8eb2e9c140d462661eefb36684d582102
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-10-13 15:51:21 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -168,12 +168,12 @@ resource "aws_iam_role" "ssm_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "ssm_attachment" {
-  role       = var.ssm_role
+  role       = aws_iam_role.ssm_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 resource "aws_iam_instance_profile" "ssm_instance_profile" {
-  name = var.ssm_instance_profile
+  name = aws_iam_role.ssm_role.name
   role = var.ssm_role
 }
 
