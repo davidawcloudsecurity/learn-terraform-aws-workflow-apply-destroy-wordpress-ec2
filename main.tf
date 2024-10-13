dfdc274f7adf2d796edd83528bf71c2658819852
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-10-13 12:46:34 +0800

    Update main.tf - change using   name = data.aws_iam_role.existing_role.name

--- main.tf
+++ main.tf
@@ -146,12 +146,11 @@ resource "aws_security_group" "db" {
 # Look for the existing role
 data "aws_iam_role" "existing_role" {
   name = var.ssm_role
-  count = var.ssm_role != "" ? 1 : 0
 }
 
 resource "aws_iam_role" "ssm_role" {
-  count = var.ssm_role != "" && length(data.aws_iam_role.existing_role) == 0 ? 1 : 0
-  name = var.ssm_role
+  count = length(data.aws_iam_role.existing_role.*.id) == 0 ? 1 : 0
+  name = data.aws_iam_role.existing_role.name
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -168,15 +167,13 @@ resource "aws_iam_role" "ssm_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "ssm_attachment" {
-  count      = var.ssm_role != "" && length(data.aws_iam_role.existing_role) == 0 ? 1 : 0
-  role       = aws_iam_role.ssm_role[0].name
+  role       = aws_iam_role.ssm_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 resource "aws_iam_instance_profile" "ssm_instance_profile" {
-  count = var.ssm_role != "" ? 1 : 0
   name = var.ssm_instance_profile
-  role = length(data.aws_iam_role.existing_role) > 0 ? data.aws_iam_role.existing_role[0].name : aws_iam_role.ssm_role[0].name
+  role = aws_iam_role.ssm_role.name
 }
 
 output "seeds" {
